732f0dc1ff4693b15f300af4ce04446beb8c078a
kxqzhou <skz.zxc@gmail.com>
2016-10-11 01:48:46 -0400

    spacing

--- src/ModeSelect.js
+++ src/ModeSelect.js
@@ -12,37 +12,37 @@ var ModeSelectLayer = cc.Layer.extend({
 		// black bg, option change color later?
 		var background = new cc.LayerColor( cc.color(0, 0, 0) );
 		// using default anchor point 0.5, 0.5 
-        // default same size as screen b/c layer
-        // default x, y = 0, 0
-        this.addChild( background, 0, TAG_BACKGROUND );
-        // it seems tag_bg = 1 is global after all, first def in MainMenu.js 
+		// default same size as screen b/c layer
+		// default x, y = 0, 0
+		this.addChild( background, 0, TAG_BACKGROUND );
+		// it seems tag_bg = 1 is global after all, first def in MainMenu.js 
 
-        var titleLabel = new cc.LabelBMFont("Mode Select", res.arial_48_fnt, winSize.width, cc.TEXT_ALIGNMENT_CENTER);
-        titleLabel.x = winSize.width / 2;
-        titleLabel.y = winSize.height / 10 * 8;
-        this.addChild( titleLabel, 2 );
+		var titleLabel = new cc.LabelBMFont("Mode Select", res.arial_48_fnt, winSize.width, cc.TEXT_ALIGNMENT_CENTER);
+		titleLabel.x = winSize.width / 2;
+		titleLabel.y = winSize.height / 10 * 8;
+		this.addChild( titleLabel, 2 );
 
-        var colorLabel = new cc.LabelBMFont("Colors", res.arial_32_fnt, winSize.width, cc.TEXT_ALIGNMENT_CENTER);
-        var colorChoice = new cc.MenuItemLabel( colorLabel, this.onModeSelect, this );
-		colorChoice.x = winSize.width / 2;
-        colorChoice.y = winSize.height / 10 * 6;
+		var colorLabel = new cc.LabelBMFont("Colors", res.arial_32_fnt, winSize.width, cc.TEXT_ALIGNMENT_CENTER);
+		var colorChoice = new cc.MenuItemLabel( colorLabel, this.onModeSelect, this );
+			colorChoice.x = winSize.width / 2;
+		colorChoice.y = winSize.height / 10 * 6;
 
-        var tempLabel = new cc.LabelBMFont("Coming soon", res.arial_32_fnt, winSize.width, cc.TEXT_ALIGNMENT_CENTER);
-        var tempMessage = new cc.MenuItemLabel( tempLabel );
-        // it seems the selector and target are optional? or have defaults
-		tempMessage.x = winSize.width / 2;
-        tempMessage.y = winSize.height / 10 * 4;
-        tempMessage.setColor( cc.color(100, 100, 100) );
+		var tempLabel = new cc.LabelBMFont("Coming soon", res.arial_32_fnt, winSize.width, cc.TEXT_ALIGNMENT_CENTER);
+		var tempMessage = new cc.MenuItemLabel( tempLabel );
+		// it seems the selector and target are optional? or have defaults
+			tempMessage.x = winSize.width / 2;
+		tempMessage.y = winSize.height / 10 * 4;
+		tempMessage.setColor( cc.color(100, 100, 100) );
 
-        var backLabel = new cc.LabelBMFont("Back", res.arial_48_fnt, winSize.width, cc.TEXT_ALIGNMENT_CENTER);
-        var backButton = new cc.MenuItemLabel( backLabel, this.onBackPress, this );
-        backButton.x = winSize.width / 2;
-        backButton.y = winSize.height / 6;
+		var backLabel = new cc.LabelBMFont("Back", res.arial_48_fnt, winSize.width, cc.TEXT_ALIGNMENT_CENTER);
+		var backButton = new cc.MenuItemLabel( backLabel, this.onBackPress, this );
+		backButton.x = winSize.width / 2;
+		backButton.y = winSize.height / 6;
 
    		var menu = new cc.Menu(colorChoice, tempMessage, backButton);
-        menu.x = 0;
-        menu.y = 0;
-        this.addChild( menu, 3 );
+		menu.x = 0;
+		menu.y = 0;
+		this.addChild( menu, 3 );
 	},
 
 	onModeSelect:function (target) {
