6b4ee0fe51fe1f6f6ae0fb7f389f50e5bf6c9160
kxqzhou <skz.zxc@gmail.com>
2016-10-11 01:47:11 -0400

    more weird spacing

--- src/Difficulty.js
+++ src/Difficulty.js
@@ -11,46 +11,46 @@ var DifficultyLayer = cc.Layer.extend({
 		var winSize = cc.director.getWinSize();
 
 		var background = new cc.LayerColor( cc.color(0, 0, 0) );
-        this.addChild( background, 0, TAG_BACKGROUND );
+		this.addChild( background, 0, TAG_BACKGROUND );
 
-        var titleText = new cc.LabelBMFont("Choose a difficulty", res.arial_48_fnt, winSize.width, cc.TEXT_ALIGNMENT_CENTER);
-        titleText.x = winSize.width / 2;
-        titleText.y = winSize.height / 5 * 4;
-        this.addChild( titleText, 2 );
+		var titleText = new cc.LabelBMFont("Choose a difficulty", res.arial_48_fnt, winSize.width, cc.TEXT_ALIGNMENT_CENTER);
+		titleText.x = winSize.width / 2;
+		titleText.y = winSize.height / 5 * 4;
+		this.addChild( titleText, 2 );
 
-        var easyLabel = new cc.LabelBMFont("Unlikely", res.arial_32_fnt, winSize.width, cc.TEXT_ALIGNMENT_CENTER);
-        var easyButton = new cc.MenuItemLabel( easyLabel, this.onDifficultyPress, this );
-        easyButton.x = winSize.width / 2;
-        easyButton.y = winSize.height / 10 * 6;
-        easyButton.tag = 1;
+		var easyLabel = new cc.LabelBMFont("Unlikely", res.arial_32_fnt, winSize.width, cc.TEXT_ALIGNMENT_CENTER);
+		var easyButton = new cc.MenuItemLabel( easyLabel, this.onDifficultyPress, this );
+		easyButton.x = winSize.width / 2;
+		easyButton.y = winSize.height / 10 * 6;
+		easyButton.tag = 1;
 
-        var mediumLabel = new cc.LabelBMFont("Improbable", res.arial_32_fnt, winSize.width, cc.TEXT_ALIGNMENT_CENTER);
-        var mediumButton = new cc.MenuItemLabel( mediumLabel, this.onDifficultyPress, this );
-        mediumButton.x = winSize.width / 2;
-        mediumButton.y = winSize.height / 2;
-        mediumButton.tag = 2;
+		var mediumLabel = new cc.LabelBMFont("Improbable", res.arial_32_fnt, winSize.width, cc.TEXT_ALIGNMENT_CENTER);
+		var mediumButton = new cc.MenuItemLabel( mediumLabel, this.onDifficultyPress, this );
+		mediumButton.x = winSize.width / 2;
+		mediumButton.y = winSize.height / 2;
+		mediumButton.tag = 2;
 
-        var hardLabel = new cc.LabelBMFont("Ridiculous", res.arial_32_fnt, winSize.width, cc.TEXT_ALIGNMENT_CENTER);
-        var hardButton = new cc.MenuItemLabel( hardLabel, this.onDifficultyPress, this );
-        hardButton.x = winSize.width / 2;
-        hardButton.y = winSize.height / 10 * 4;
-        hardButton.tag = 3;
+		var hardLabel = new cc.LabelBMFont("Ridiculous", res.arial_32_fnt, winSize.width, cc.TEXT_ALIGNMENT_CENTER);
+		var hardButton = new cc.MenuItemLabel( hardLabel, this.onDifficultyPress, this );
+		hardButton.x = winSize.width / 2;
+		hardButton.y = winSize.height / 10 * 4;
+		hardButton.tag = 3;
 
-        var extremeLabel = new cc.LabelBMFont("Absurd", res.arial_32_fnt, winSize.width, cc.TEXT_ALIGNMENT_CENTER);
-        var extremeButton = new cc.MenuItemLabel( extremeLabel, this.onDifficultyPress, this );
-        extremeButton.x = winSize.width / 2;
-        extremeButton.y = winSize.height / 10 * 3;
-        extremeButton.tag = 4;
+		var extremeLabel = new cc.LabelBMFont("Absurd", res.arial_32_fnt, winSize.width, cc.TEXT_ALIGNMENT_CENTER);
+		var extremeButton = new cc.MenuItemLabel( extremeLabel, this.onDifficultyPress, this );
+		extremeButton.x = winSize.width / 2;
+		extremeButton.y = winSize.height / 10 * 3;
+		extremeButton.tag = 4;
 
-        var backLabel = new cc.LabelBMFont("Back", res.arial_48_fnt, winSize.width, cc.TEXT_ALIGNMENT_CENTER);
-        var backButton = new cc.MenuItemLabel( backLabel, this.onBackPress, this );
-        backButton.x = winSize.width / 2;
-        backButton.y = winSize.height / 7;
+		var backLabel = new cc.LabelBMFont("Back", res.arial_48_fnt, winSize.width, cc.TEXT_ALIGNMENT_CENTER);
+		var backButton = new cc.MenuItemLabel( backLabel, this.onBackPress, this );
+		backButton.x = winSize.width / 2;
+		backButton.y = winSize.height / 7;
 
-        var menu = new cc.Menu(easyButton, mediumButton, hardButton, extremeButton, backButton);
-        menu.x = 0;
-        menu.y = 0;
-        this.addChild( menu, 3 );
+		var menu = new cc.Menu(easyButton, mediumButton, hardButton, extremeButton, backButton);
+		menu.x = 0;
+		menu.y = 0;
+		this.addChild( menu, 3 );
 	},
 	onDifficultyPress:function (target) {
 		var len = 10;
