012c93ef4be684c6be5cbf978186bfa2f74bb738
kxqzhou <skz.zxc@gmail.com>
2016-10-11 01:46:25 -0400

    hopefully fixed weird spacing issue

--- src/MainMenu.js
+++ src/MainMenu.js
@@ -14,46 +14,46 @@ var MainMenuLayer = cc.Layer.extend({
 		// black bg, option change color later?
 		var background = new cc.LayerColor( cc.color(0, 0, 0) );
 		// using default anchor point 0.5, 0.5 
-        // default same size as screen b/c layer
-        // default x, y = 0, 0
-        this.addChild( background, 0, TAG_BACKGROUND );
+		// default same size as screen b/c layer
+		// default x, y = 0, 0
+		this.addChild( background, 0, TAG_BACKGROUND );
 
-        // var titleText = new cc.LabelTTF("Memo", "Arial", 48, cc.TEXT_ALIGNMENT_CENTER);
-        // using default cc.VERTICAL_TEXT_ALIGNMENT_TOP
-        var titleText = new cc.LabelBMFont("Memo", res.arial_48_fnt, winSize.width, cc.TEXT_ALIGNMENT_CENTER);
-        titleText.x = winSize.width / 2;
-        titleText.y = winSize.height / 4 * 3;
-        // font color default white apparently
-        this.addChild( titleText, 2 );
+		// var titleText = new cc.LabelTTF("Memo", "Arial", 48, cc.TEXT_ALIGNMENT_CENTER);
+		// using default cc.VERTICAL_TEXT_ALIGNMENT_TOP
+		var titleText = new cc.LabelBMFont("Memo", res.arial_48_fnt, winSize.width, cc.TEXT_ALIGNMENT_CENTER);
+		titleText.x = winSize.width / 2;
+		titleText.y = winSize.height / 4 * 3;
+		// font color default white apparently
+		this.addChild( titleText, 2 );
 
-        // color tests
-        // titleText.color = new cc.Color(100, 100, 100); 
+		// color tests
+		// titleText.color = new cc.Color(100, 100, 100); 
 
-        /*
-        var titleBar = new cc.Sprite();
-        titleBar.x = winSize.width / 2;
-        titleBar.y = winSize.height / 100 * 80;
-        this.addChild(titleBar, 3);*/
-        
+		/*
+		var titleBar = new cc.Sprite();
+		titleBar.x = winSize.width / 2;
+		titleBar.y = winSize.height / 100 * 80;
+		this.addChild(titleBar, 3);*/
 
-        var startLabel = new cc.LabelBMFont("Start", res.arial_32_fnt, winSize.width, cc.TEXT_ALIGNMENT_CENTER);
-        var startButton = new cc.MenuItemLabel( startLabel, this.onStartPress, this );
-        startButton.x = winSize.width / 2;
-        startButton.y = winSize.height / 2;
 
-        var settingsLabel = new cc.LabelBMFont("Settings", res.arial_32_fnt, winSize.width, cc.TEXT_ALIGNMENT_CENTER);
-        var settingsButton = new cc.MenuItemLabel( settingsLabel, this.onSettingsPress, this );
-		settingsButton.x = winSize.width / 2;
-        settingsButton.y = winSize.height / 8 * 3;
+		var startLabel = new cc.LabelBMFont("Start", res.arial_32_fnt, winSize.width, cc.TEXT_ALIGNMENT_CENTER);
+		var startButton = new cc.MenuItemLabel( startLabel, this.onStartPress, this );
+		startButton.x = winSize.width / 2;
+		startButton.y = winSize.height / 2;
 
-        var menu = new cc.Menu(startButton, settingsButton);
-        menu.x = 0;
-        menu.y = 0;
-        this.addChild( menu, 3 );
+		var settingsLabel = new cc.LabelBMFont("Settings", res.arial_32_fnt, winSize.width, cc.TEXT_ALIGNMENT_CENTER);
+		var settingsButton = new cc.MenuItemLabel( settingsLabel, this.onSettingsPress, this );
+			settingsButton.x = winSize.width / 2;
+		settingsButton.y = winSize.height / 8 * 3;
 
-        if (!cc.audioEngine.isMusicPlaying()) {
-        	cc.audioEngine.playMusic(res.menu_bgm, true);
-        }
+		var menu = new cc.Menu(startButton, settingsButton);
+		menu.x = 0;
+		menu.y = 0;
+		this.addChild( menu, 3 );
+
+		if (!cc.audioEngine.isMusicPlaying()) {
+			cc.audioEngine.playMusic(res.menu_bgm, true);
+		}
 	},
 
 	onStartPress:function () {
